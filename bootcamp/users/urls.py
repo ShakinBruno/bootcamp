fb6189366c2c949e7f4613ace77b80f78dfa9ace
ShakinBruno <dominik.mikolajczyyk@gmail.com>
2024-04-02 00:54:45 +0200

    Fixed users URLs

--- bootcamp/users/urls.py
+++ bootcamp/users/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    re_path(regex=r"^$", view=views.UserListView.as_view(), name="list"),
-    re_path(regex=r"^~redirect/$", view=views.UserRedirectView.as_view(), name="redirect"),
-    re_path(regex=r"^~update/$", view=views.UserUpdateView.as_view(), name="update"),
+    re_path(r"^$", views.UserListView.as_view(), name="list"),
+    re_path(r"^~redirect/$", views.UserRedirectView.as_view(), name="redirect"),
+    re_path(r"^~update/$", views.UserUpdateView.as_view(), name="update"),
     re_path(
-        regex=r"^(?P<username>[\w.@+-]+)/$",
-        view=views.UserDetailView.as_view(),
+        r"^(?P<username>[\w.@+-]+)/$",
+        views.UserDetailView.as_view(),
         name="detail",
     ),
 ]
