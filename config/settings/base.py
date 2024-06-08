222fb0c524df34e958bb7250445d16dcd4b5253c
ShakinBruno <dominik.mikolajczyyk@gmail.com>
2024-06-09 01:58:13 +0200

    changed http server and added messages compression

--- config/settings/base.py
+++ config/settings/base.py
@@ -145,6 +145,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.gzip.GZipMiddleware",
     "allauth.account.middleware.AccountMiddleware",
 ]
 
